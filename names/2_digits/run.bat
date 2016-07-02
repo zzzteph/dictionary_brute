05d0ec8f6b81b3b5c179879e7137ba5bfbfbd0d4
steph <stephanzag@gmail.com>
2016-07-02 16:09:08 +0300

    names rules update

---
+++ names/2_digits/run.bat
@@ -0,0 +1,1 @@
+D:\hashcat\hashcat64.exe -d 1 -o hash.result --potfile-disable -m 0 test.hash -r two_digits.rules ..\names.dic
---
+++ names/6_digits/run.bat
@@ -0,0 +1,2 @@
+D:\hashcat\hashcat64.exe -d 1 -o hash.result --potfile-disable -m 0 test.hash -r ddmmyy.rules ..\names.dic
+D:\hashcat\hashcat64.exe -d 1 -o hash.result --potfile-disable -m 0 test.hash -r yyyymm.rules ..\names.dic
