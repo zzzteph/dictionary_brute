fdfcabb9d98ec8bcbe7f0ef237431a1f269349d1
tsmall888 <nautim@ya.ru>
2016-07-12 17:05:56 +0500

    var replaced by diff

--- tools/diff.py
+++ tools/diff.py
@@ -5,8 +5,8 @@ import sys
 
 with open(sys.argv[1], 'r') as f1:
 	with open(sys.argv[2], 'r') as f2:
-		same = set(f1).difference(f2)
+		diff = set(f1).difference(f2)
 
 with open('diff.txt', 'w') as outfile:
-	for line in same:
+	for line in diff:
 		outfile.write(line)
